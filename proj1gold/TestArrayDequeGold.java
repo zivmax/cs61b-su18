5bf467f2d1241a0e2a2e75146917abeb43958281
ziv <108971427+zivmax@users.noreply.github.com>
2023-09-27 19:09:23 +0800

    Sync

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -10,32 +10,28 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
         String msg = "";
-        
+
         for (int i = 0; i < 1000; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne < 0.3) {
                 sad1.addLast(i);
                 sad2.addLast(i);
-                msg = msg + "addLast(" + i + ")\n"; 
-            } else if (numberBetweenZeroAndOne > 0.6) {
-                sad1.removeFirst();
-                sad2.removeFirst();
-                msg = msg + "removeFirst()\n"; 
-            } else if (numberBetweenZeroAndOne > 0.8) {
-                sad1.removeLast();
-                sad2.removeLast();
-                msg = msg + "removeFirst()\n"; 
-            } else {
+                msg = msg + "addLast(" + i + ")\n";
+            } else if (numberBetweenZeroAndOne <= 0.6) {
                 sad1.addFirst(i);
                 sad2.addFirst(i);
-                msg = msg + "addFirst(" + i + ")\n"; 
-            }
-            for (int j = 0; j < Math.min(sad1.size(), sad2.size()); j++) {
-                assertEquals(msg, sad1.get(j), sad2.get(j));
+                msg = msg + "addFirst(" + i + ")\n";
+            } else if (numberBetweenZeroAndOne > 0.6) {
+                msg = msg + "removeFirst()\n";
+                assertEquals(msg, sad1.removeFirst(), sad2.removeFirst());
+            } else if (numberBetweenZeroAndOne > 0.8) {
+                msg = msg + "removeFirst()\n";
+                assertEquals(msg, sad1.removeLast(), sad2.removeLast());
             }
         }
     }
+
     public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests(TestArrayDequeGold.class);
     }
